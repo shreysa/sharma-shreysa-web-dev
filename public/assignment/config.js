d5aba67f58a215296b138a1271e78df496c4ed03
sharma.sh <sharma.sh@husky.neu.edu>
2016-05-25 17:16:02 -0400

    renamed files, added controller in profile and login page

---
+++ public/assignment/config.js
@@ -0,0 +1,55 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+    
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
+            })
+            .when("/profile/:id", {
+            templateUrl: "views/user/profile.view.client.html",
+                controller:"ProfileController",
+                controllerAs: "model"
+        })
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
+            })
+            .when("/page-edit", {
+                templateUrl: "views/page/page-edit.view.client.html"
+            })
+            .when("/page-list", {
+                templateUrl: "views/page/page-list.view.client.html"
+            })
+            .when("/page-new", {
+                templateUrl: "views/page/page-new.view.client.html"
+            })
+            .when("/website-edit", {
+                templateUrl: "views/website/website-edit.view.client.html"
+            })
+            .when("/website-list", {
+                templateUrl: "views/website/website-list.view.client.html"
+            })
+            .when("/website-new", {
+                templateUrl: "views/website/website-new.view.client.html"
+            })
+            .when("/widget-chooser", {
+                templateUrl: "views/widget/widget-chooser.view.client.html"
+            })
+            .when("/widget-heading", {
+                templateUrl: "views/widget/widget-heading.view.client.html"
+            })
+            .when("/widget-image", {
+                templateUrl: "views/widget/widget-image.view.client.html"
+            })
+            .when("/widget-list", {
+                templateUrl: "views/widget/widget-list.view.client.html"
+            })
+            .when("/widget-youtube", {
+                templateUrl: "views/widget/widget-youtube.view.client.html"
+            });
+    }
+})();
---
+++ public/assignment/views/user/user.controller.client.js
@@ -0,0 +1,53 @@
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("LoginController", LoginController)
+        .controller("ProfileController", ProfileController);
+
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
+    function ProfileController($routeParams) {
+       var vm = this;
+        var id = $routeParams["id"];
+        for (var i in users) {
+            if(users[i]._id === id) {
+                vm.user = users[i];
+            }
+
+
+        }
+        console.log(id);
+    }
+
+    function LoginController($location) {
+
+
+        var vm = this;
+        vm.login = login;
+
+            function login (username, password) {
+                 console.log(username);
+                 console.log(password);
+
+                for (var i in users)
+                {
+                    if (users[i].username === username && users[i].password === password) {
+                        var id = users[i]._id;
+                        $location.url("/profile/" + id);
+                    } else {
+                        vm.error = "User not found";
+                    }
+
+                }
+            
+        }
+
+    }
+
+
+})();
