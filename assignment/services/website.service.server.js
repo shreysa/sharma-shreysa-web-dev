e2fa251224ad4293fe0486e2ed2e1547ace28ec1
sharma.sh <sharma.sh@husky.neu.edu>
2016-06-02 02:06:51 -0400

    added website server service

---
+++ assignment/services/website.service.server.js
@@ -0,0 +1,26 @@
+module.exports = function (app) {
+    var websites = [
+        { "_id": "123", "name": "Facebook",    "developerId": "456" },
+        { "_id": "234", "name": "Tweeter",     "developerId": "456" },
+        { "_id": "456", "name": "Gizmodo",     "developerId": "456" },
+        { "_id": "567", "name": "Tic Tac Toe", "developerId": "123" },
+        { "_id": "678", "name": "Checkers",    "developerId": "123" },
+        { "_id": "789", "name": "Chess",       "developerId": "234" }
+    ];
+
+    app.get("/api/user/:userId/website", findAllWebsitesForUser);
+
+    function findAllWebsitesForUser(req, res) {
+        var result = [];
+        var id = req.params.userId;
+        for(var w in websites){
+            if(websites[w].developerId === id){
+                result.push(websites[w]);
+            }
+        }
+        res.json(result)
+        return;
+    }
+    
+    
+}
