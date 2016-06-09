b73bbf9c0ca11ad71eb66dcb40d6ac24c6d87a3b
sharma.sh <sharma.sh@husky.neu.edu>
2016-06-09 16:48:30 -0400

    user model completely implemented

---
+++ assignment/models/models.js
@@ -0,0 +1,15 @@
+module.exports = function () {
+    var userModel;
+    var websiteModel;
+    var pageModel;
+    var widgetModel;
+
+    var models= {
+        userModel: userModel,
+        websiteModel: websiteModel,
+        pageModel: pageModel,
+        widgetModel: widgetModel
+    };
+
+    return models;
+};
